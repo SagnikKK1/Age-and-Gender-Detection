c1f1938067273d847dce386e2641c757d14045ad
swayambhu2104 <122199947+swayambhu2104@users.noreply.github.com>
2024-03-26 16:43:52 +0530

    Update predict.py

--- Age_Prediction_1/predict.py
+++ Age_Prediction_1/predict.py
@@ -194,6 +194,7 @@ if (args.type=='video'):
             x1, y1, x2, y2 = int(boxes[j][0]), int(boxes[j][1]), int(boxes[j][2]), int(boxes[j][3])
             gender_pred, age_pred = get_pred_attributes(frame, x1, y1, x2, y2)
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), thickness=5)
+            exact_age = (age_pred[0] + age_pred[1])/2
             cv2.putText(frame, f"AGE: {exact_age}", (x1, y1 - 10),
                       cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), thickness=2)
             exact_age = (age_pred[0] + age_pred[1])/2
@@ -238,84 +239,7 @@ if (args.type=='video'):
 
     df.to_csv(f'prediction/{vid_filename}/prediction.csv')
 
-    os.chdir(f'prediction/{vid_filename}/images')
-    path = f'prediction/{vid_filename}/images'
-    mean_height = 0
-    mean_width = 0
-
-    num_of_images = len(os.listdir(path))
-    # print(num_of_images)
-
-    for file in os.listdir(path):
-        im = Image.open(os.path.join(path, file))
-        width, height = im.size
-        mean_width += width
-        mean_height += height
-        # im.show()   # uncomment this for displaying the image
-
-    # Finding the mean height and width of all images.
-    # This is required because the video frame needs
-    # to be set with same width and height. Otherwise
-    # images not equal to that width height will not get
-    # embedded into the video
-    mean_width = int(mean_width / num_of_images)
-    mean_height = int(mean_height / num_of_images)
-
-    # print(mean_height)
-    # print(mean_width)
-
-    # Resizing of the images to give
-    # them same width and height
-    for file in os.listdir(path):
-        if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith("png"):
-            # opening image using PIL Image
-            im = Image.open(os.path.join(path, file))
-
-            # im.size includes the height and width of image
-            width, height = im.size
-            print(width, height)
-
-            # resizing
-            imResize = im.resize((mean_width, mean_height), Image.ANTIALIAS)
-            imResize.save( file, 'JPEG', quality = 95) # setting quality
-            # printing each resized image name
-            print(im.filename.split('\\')[-1], " is resized")
-
-
-    # Video Generating function
-    def generate_video():
-        image_folder = path # make sure to use your folder
-        video_name = 'saved_video1.avi'
-        os.chdir("video")
-
-        images = [img for img in os.listdir(image_folder)
-                if img.endswith(".jpg") or
-                    img.endswith(".jpeg") or
-                    img.endswith("png")]
-
-        # Array images should only consider
-        # the image files ignoring others if any
-        print(images)
-
-        frame = cv2.imread(os.path.join(image_folder, images[0]))
-
-        # setting the frame width, height width
-        # the width, height of first image
-        height, width, layers = frame.shape
-
-        video = cv2.VideoWriter(video_name, 0, 20, (width, height))
-
-        # Appending the images to the video one by one
-        for image in images:
-            video.write(cv2.imread(os.path.join(image_folder, image)))
-
-        # Deallocating memories taken for window creation
-        cv2.destroyAllWindows()
-        video.release()  # releasing the video generated
-
-
-    # Calling the generate_video function
-    generate_video()
+   
 ## Prediction For Images
 
 if (args.type=='image'):
